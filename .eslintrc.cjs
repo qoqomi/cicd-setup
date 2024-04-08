f4cbbfdbaa1ef7657cedd4ec9072490248a1c270
qoqomi <energyy044@gmail.com>
2024-04-08 21:13:47 +0900

    add format and lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,34 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    ".lintstagedrc.cjs",
+    "commitlint.config.cjs",
+    "jest.config.js",
+    "vite.config.js",
+    "vitest.setup.js",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react-refresh", "prettier"],
+  settings: { react: { version: "18.2" } },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
